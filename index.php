f26affe6202decd21a5c28227404f24f23bad097
Tom McPadden <tommcpadden1@gmail.com>
2013-11-29 04:09:08 +0000

    Update index.php

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?
 
-//Call external file/class 
+//Call external file/class
 require 'lookAtMyCode.php';
 
 //Create object from class
@@ -20,7 +20,7 @@ $lamc = new lookAtMyCode();
 	<body>
 		<?
 		
-		//Compile and display the code box 
+		//Compile and display the code box
 		$lamc->compile(
 			array(
 				"codes/project1/html.html",
